dea7f5757913bcfe7713a90be36f3eab7baa2f55
Jojo94 <31824471+Jojo94@users.noreply.github.com>
2017-11-03 14:02:27 +0100

    Es fliegt von alleine mit Turbolader

--- autopilot.js
+++ autopilot.js
@@ -3,7 +3,8 @@
  * @author alteredq / http://alteredqualia.com/
  * @author paulirish / http://paulirish.com/
  */
-
+var stepsize = 1 / 3600;var to = 0;
+    var tf = 6;
 THREE.FirstPersonControls = function ( object, domElement ) {
 
 	this.object = object;
@@ -73,8 +74,8 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 		}
 
 	};
-/*
-	this.onMouseDown = function ( event ) {
+
+/*	this.onMouseDown = function ( event ) {
 
 		if ( this.domElement !== document ) {
 
@@ -134,22 +135,150 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 		}
 
-	};
+	};*/
+ aero = function () 
+{
 
+    var S = 0.017;
+    var AR = 0.86;
+    var e = 0.9;
+    var m = 0.003;
+    var g = 9.8;
+    var rho = 1.225;
+    var CLa = Math.PI * AR / (1 + Math.sqrt(1 + Math.pow((AR / 2), 2)));
+    var CDo = 0.02;
+    var epsilon = 1 / (Math.PI * e * AR);
+    var CL = Math.sqrt(CDo / epsilon);
+    var CD = CDo + epsilon * Math.pow(CL, 2);
+    var LDmax = CL / CD;
+    var Gam = -Math.atan(1 / LDmax);
+    var V = Math.sqrt(2 * m * g / (rho * S * (CL * Math.cos(Gam) - CD * Math.sin(Gam))));
+    var Alpha = CL / CLa;
+    var q = 0.5 * rho * Math.pow(V, 2);
+
+    var H = 2;
+    var R = 0;
+    var to = 0;
+    var tf = 6;
+    var xo = [];
+    xo[0] = V;
+    xo[1] = Gam;
+    xo[2] = H;
+    xo[3] = R;
+
+    var stepsize = 1 / 3600;
+    var s1 = [];
+    var s2 = [];
+    var s3 = [];
+    var tn = [];
+    var xn = [];
+    for (var i = 0; i < 4; i++)
+    {
+        xn[i] = [];
+    }
+
+    tn[0] = to;
+    for (var i = 0; i < 4; i++)
+    {
+        xn[i][0] = xo[i];
+    }
+
+    for (var i = 1; i <= (1 / stepsize * (tf - to) + 1); i++)
+    {
+        tn[i] = to + i * stepsize;
+
+        q = 0.5 * rho * Math.pow(xn[0][i - 1], 2);
+
+        s1[0] = (-1 * CD * q * S - m * g * Math.sin(xn[1][i - 1])) / m;
+        s1[1] = (CL * q * S - m * g * Math.cos(xn[1][i - 1])) / (m * xn[0][i - 1]);
+        s1[2] = xn[0][i - 1] * Math.sin(xn[1][i - 1]);
+        s1[3] = xn[0][i - 1] * Math.cos(xn[1][i - 1]);
+
+        s2[0] = (-1 * CD * q * S - m * g * Math.sin(xn[1][i - 1] + (stepsize / 2) * s1[1])) / m;
+        s2[1] = (CL * q * S - m * g * Math.cos(xn[1][i - 1] + (stepsize / 2) * s1[1])) / (m * (xn[0][i - 1] + (stepsize / 2) * s1[0]));
+        s2[2] = (xn[0][i - 1] + (stepsize / 2) * s1[0]) * Math.sin(xn[1][i - 1] + (stepsize / 2) * s1[1]);
+        s2[3] = (xn[0][i - 1] + (stepsize / 2) * s1[0]) * Math.cos(xn[1][i - 1] + (stepsize / 2) * s1[1]);
+
+        s3[0] = (-1 * CD * q * S - m * g * Math.sin(xn[1][i - 1] + (stepsize / 4) * 3 * s2[1])) / m;
+        s3[1] = (CL * q * S - m * g * Math.cos(xn[1][i - 1] + (stepsize / 4) * 3 * s2[1])) / (m * (xn[0][i - 1] + (stepsize / 4) * 3 * s2[0]));
+        s3[2] = (xn[0][i - 1] + (stepsize / 4) * 3 * s2[0]) * Math.sin((xn[1][i - 1] + (stepsize / 4) * 3 * s2[1]));
+        s3[3] = (xn[0][i - 1] + (stepsize / 4) * 3 * s2[0]) * Math.cos((xn[1][i - 1] + (stepsize / 4) * 3 * s2[1]));
+
+        for (var j = 0; j < 4; ++j)
+        {
+            xn[j][i] = xn[j][i - 1] + (stepsize / 9) * (2 * s1[j] + 3 * s2[j] + 4 * s3[j]);
+        }
+    }
+
+    
+return xn;
+};
+var xn  = aero();
+
+ 
+var Vn = [];
+    for (var i = 0; i <= 1 / stepsize * (tf - to) + 1; i++)
+    {
+        Vn[i] = xn[0][i];
+    }
+    var Gamn = [];
+    for (var i = 0; i <= 1 / stepsize * (tf - to) + 1; i++)
+    {
+        Gamn[i] = xn[1][i];
+    }
+    var Hn = [];
+    for (var i = 0; i <= 1 / stepsize * (tf - to) + 1; i++)
+    {
+        Hn[i] = xn[2][i]+1.8;
+    }
+    var Rn = [];
+    for (var i = 0; i <= 1 / stepsize * (tf - to) + 1; i++)
+    {
+        Rn[i] = xn[3][i];
+    }
+var i = 0;
+
+  function flug()
+{
+	camera.position.x = 400*Rn[100*i];
+	camera.position.y = 400*Hn[100*i];
+	if(i<21000){				  
+	  setTimeout(flug,1);
+	  i++;}
+}
 	this.onKeyDown = function ( event ) {
+
 
 		//event.preventDefault();
 
 		switch ( event.keyCode ) {
 
 			case 38: 
-			case 87:  this.moveForward = true; break;
+			//case 87:  this.moveForward = true; break;
+			case 87:  
+				  flug();
+				  break;
 
 			case 37: 
-			case 65:  this.moveLeft = true; break;
+			case 65:  	
+this.lat= 1.76*180/Math.PI;
+this.phi = THREE.Math.degToRad(this.lat );
+this.theta = THREE.Math.degToRad( this.lon );
+var targetPosition = this.target,
+			position = this.object.position;
+
+		targetPosition.x = position.x + 100 * Math.sin( this.phi ) * Math.cos( this.theta );
+		targetPosition.y = position.y + 100 * Math.cos( this.phi );
+		targetPosition.z = position.z + 100 * Math.sin( this.phi ) * Math.sin( this.theta );
+
+		this.object.lookAt( targetPosition );
+    this.movementSpeed = 350;
+    this.moveForward = true;
+
+break;
 
 			case 40: 
-			case 83:  this.moveBackward = true; break;
+			case 83:  this.movementSpeed += 50; break;
 
 			case 39: 
 			case 68:  this.moveRight = true; break;
@@ -182,8 +311,8 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 		}
 
-	};*/
-  aero = function () 
+	};
+ /* aero = function () 
 {
 
     var S = 0.017;
@@ -206,7 +335,7 @@ THREE.FirstPersonControls = function ( object, domElement ) {
     var H = 2;
     var R = 0;
     var to = 0;
-    var tf = 6;
+    var tf = 1;
     var xo = [];
     xo[0] = 3*V;
     xo[1] = Gam;
@@ -277,9 +406,9 @@ THREE.FirstPersonControls = function ( object, domElement ) {
     {
         Rn[i] = xn[3][i];
     }
-};
+}; */
 
-	this.update = function( delta ) {
+	this.update = function( delta ) { // delta ist Zeitspanne zwischen letzter und jetztiger Aktualisierung?
 
 		if ( this.enabled === false ) return;
 
@@ -315,36 +444,7 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 		}
 
-		var verticalLookRatio = 1;
-
-		if ( this.constrainVertical ) {
-
-			verticalLookRatio = Math.PI / ( this.verticalMax - this.verticalMin );
-
-		}
-
-		this.lon += this.mouseX * actualLookSpeed;
-		if ( this.lookVertical ) this.lat -= this.mouseY * actualLookSpeed * verticalLookRatio;
-
-		this.lat = Math.max( - 85, Math.min( 85, this.lat ) );
-		this.phi = THREE.Math.degToRad( 90 - this.lat );
-
-		this.theta = THREE.Math.degToRad( this.lon );
-
-		if ( this.constrainVertical ) {
-
-			this.phi = THREE.Math.mapLinear( this.phi, 0, Math.PI, this.verticalMin, this.verticalMax );
-
-		}
-
-		var targetPosition = this.target,
-			position = this.object.position;
-
-		targetPosition.x = position.x + 100 * Math.sin( this.phi ) * Math.cos( this.theta );
-		targetPosition.y = position.y + 100 * Math.cos( this.phi );
-		targetPosition.z = position.z + 100 * Math.sin( this.phi ) * Math.sin( this.theta );
-
-		this.object.lookAt( targetPosition );
+		
 
 	};
 

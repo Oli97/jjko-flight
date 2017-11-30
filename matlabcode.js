79829d2b5f9c437e89563d6cdfc27adb7951ee30
kira1996 <31589235+kira1996@users.noreply.github.com>
2017-11-30 17:00:13 +0100

    Add files via upload

--- matlabcode.js
+++ matlabcode.js
@@ -23,11 +23,32 @@ return H;
 
 event = function(t,x)
 {
-  var value       =   x(6); 
-  var isterminal  =   1; 
+  var value       =   x(6);
+  var isterminal  =   1;
   var direction   =   1;
-  
+
   var eventausgabe = [value, isterminal, direction];
 
-return eventausgabe;  
+return eventausgabe;
+}
+
+
+/*	 RMQ computes the rotation matrix as a function of quaternions.
+The rotation matrices transform vectors from the earth-relative frame of
+reference to the body-axis frame.
+Quaternion: q1, x Component of quaternion q2, y Component of quaternion
+q3, z Component of quaternion q4, cos(Euler) Component of quaternion */
+RMQ = function(q1,q1,q3,q4)
+{
+  var H;
+	H[1][1] = q1^2 - q2^2 - q3^2 + q4^2;
+	H[1][2] = 2*(q1*q2 + q3*q4);
+	H[1][3] = 2*(q1*q3 - q2*q4);
+	H[2][1] = 2*(q1*q2 - q3*q4);
+	H[2][2] = -q1^2 + q2^2 - q3^2 + q4^2;
+	H[2][3] = 2*(q2*q3 + q1*q4);
+	H[3][1] = 2*(q1*q3 + q2*q4);
+	H[3][2] = 2*(q2*q3 - q1*q4);
+	H[3][3] = -q1^2 - q2^2 + q3^2 + q4^2;
+  return H;
 }

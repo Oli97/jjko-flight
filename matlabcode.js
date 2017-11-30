987421cdb4ec092fd32c5cb4aa5d6c213f9d3ca2
Jojo94 <31824471+Jojo94@users.noreply.github.com>
2017-11-30 16:38:54 +0100

    Add files via upload

---
+++ matlabcode.js
@@ -0,0 +1,22 @@
+DCM = function(Phi,Theta,Psi)
+{
+  var sinR = Math.sin(Phi);
+  var cosR = Math.cos(Phi);
+  var sinP = Math.sin(Theta);
+  var cosP = Math.cos(Theta);
+  var sinY = Math.sin(Psi);
+  var cosY = Math.cos(Psi);
+  var H;
+
+  H[0][0] = cosP * cosY;
+  H[1][2] = cosP * sinY;
+  H[1][3] = -sinP;
+  H[2][1] = sinR * sinP * cosY - cosR * sinY;
+  H[2][2] = sinR * sinP * sinY + cosR * cosY;
+  H[2][3] = sinR * cosP;
+  H[3][1] = cosR * sinP * cosY + sinR * sinY;
+  H[3][2] = cosR * sinP * sinY - sinR * cosY;
+  H[3][3] = cosR * cosP;
+
+return H;
+}
